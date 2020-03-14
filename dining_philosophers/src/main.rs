32151ab980620cb75192696b51a9d056a32e5db9
sochiai <sochiaig@gmail.com>
2020-03-15 01:39:42 +0900

    add dining_philosophers tutorial

---
+++ dining_philosophers/src/main.rs
@@ -0,0 +1,60 @@
+use std::thread;
+use std::time::Duration;
+use std::sync::{Mutex, Arc};
+
+struct Philosopher {
+    name: String,
+    left: usize,
+    right: usize,
+}
+
+impl Philosopher {
+    fn new(name: &str, left: usize, right: usize) -> Philosopher {
+        Philosopher {
+            name: name.to_string(),
+            left: left,
+            right: right,
+        }
+    }
+
+    fn eat(&self, table: &Table) {
+        let _left = table.forks[self.left].lock().unwrap();
+        thread::sleep(Duration::from_millis(150));
+        let _right = table.forks[self.right].lock().unwrap();
+        println!("{} is eating.", self.name);
+        thread::sleep(Duration::from_millis(1000));
+        println!("{} is done eating.", self.name);
+    }
+}
+
+struct Table {
+    forks: Vec<Mutex<()>>,
+}
+
+fn main() {
+    let table = Arc::new(Table { forks: vec![
+        Mutex::new(()),
+        Mutex::new(()),
+        Mutex::new(()),
+        Mutex::new(()),
+        Mutex::new(()),
+    ]});
+    let philosophers = vec![
+        Philosopher::new("Judith", 0, 1),
+        Philosopher::new("Gilles", 1, 2),
+        Philosopher::new("Karl", 2, 3),
+        Philosopher::new("Emma", 3, 4),
+        Philosopher::new("Michel", 0, 4),
+    ];
+
+    let handles: Vec<_> = philosophers.into_iter().map(|p| {
+        let table = table.clone();
+        thread::spawn(move || {
+            p.eat(&table);
+        })
+    }).collect();
+
+    for h in handles {
+        h.join().unwrap();
+    }
+}
